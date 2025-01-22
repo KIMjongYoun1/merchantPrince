41261a1d3fbb920fb29a2314a8e220ecb338332b
KIMjongYoun1 <oomg4297@gmail.com>
2025-01-22 18:57:30 +0900

    프로젝트시작

---
+++ merchantPrince/src/main/java/merchantPrince/MerchantPrinceApplication.java
@@ -0,0 +1,13 @@
+package merchantPrince;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantPrinceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchantPrinceApplication.class, args);
+	}
+
+}
---
+++ merchantPrince/src/main/java/user/User.java
@@ -0,0 +1,45 @@
+package user;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@ToString(exclude = "passWord")
+public class User {
+	
+	@Id
+	@Column(name = "user_id", nullable = false, unique = true)
+	private String userId;
+	
+	
+	@Column(name = "user_name", nullable = false)
+	private String userName;
+	
+	
+	@Column(name = "password", nullable = false)
+	private String passWord;
+	
+	
+	@Column(name ="email", nullable = false)
+	private String email;
+	
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+	
+	@PrePersist
+	public void prePersist() {
+		this.createdAt = LocalDateTime.now();
+	}
+	
+}
